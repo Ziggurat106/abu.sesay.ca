24a2195d03a992b0382e1c550d96b74dc7863602
Abu Sesay <aisesay@gmail.com>
2013-04-07 23:35:20 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AbuSesayCa::Application.config.secret_key_base = '45efa86b5da8f224a9459520928a63bc89d0831e095c959f513a436878213ad05686729f7922f1cde5134376bdf5d15bf0b32def73c7aa5ca1002ad7464b285a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AbuSesayCa::Application.config.session_store :encrypted_cookie_store, key: '_abu_sesay_ca_session'
